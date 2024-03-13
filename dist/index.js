8a1e7021458b175adf4b79e73956c23c9c5e19ae
Imran1z <shaikhzimran12334@gmail.com>
2024-03-13 12:31:14 +0530

    data types

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,12 @@
 "use strict";
-const s = 33;
-console.log(s);
+//Declaring the variables in typescript
+let a = 24;
+let b = "Imran";
+let c = true;
+//This is just conersion or another way of writing it
+let d = 24;
+let e = "Imran";
+let f = true;
+//union of two datatypes
+let surname = "Ahmed";
+surname = 5;
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-const s =33;
-console.log(s)
+
+//Declaring the variables in typescript
+let a:number=24;
+let b:string="Imran";
+let c:boolean=true;
+
+//This is just conersion or another way of writing it
+let d=<number>24;
+let e=<string>"Imran";
+let f=<boolean>true;
+
+//union of two datatypes
+let surname: string|number="Ahmed";
+surname=5;
+
